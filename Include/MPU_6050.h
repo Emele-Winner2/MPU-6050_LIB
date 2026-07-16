fc4fad6da92895fac315eb5817da2d841ef51568
Emele-Winner2 <winneremele6@gmail.com>
2026-07-16 10:20:34 +0100

    changed function implementation to be of type void instead of int, as the function does not return any value.

--- Include/MPU_6050.h
+++ Include/MPU_6050.h
@@ -10,13 +10,13 @@
 #include <main.h>
 #include <math.h>
 #include <stdio.h>
-#define MPU_ADDR (0x68<<1)
+#define MPU_ADDR (0x68 << 1)
 #define WHO_AM_I (0x75)
 #define WAKE (0x6B)
 #define ALPHA (0.98f)
 #define READINGS (0x3B)
 #define GYRO_CONFIG (0x1B)
 #define ACCEL_CONFIG (0x1C)
-uint8_t MPU6050_Init(I2C_HandleTypeDef *hi2c,uint8_t GYRO_MODE,uint8_t ACCEL_MODE);//Initializing...
-uint8_t MPU_6050_POSITION(I2C_HandleTypeDef *hi2c,uint8_t raw_data[14],uint8_t GYRO_MODE);//using Complementary Filters
-#endif /* INC_MPU_6050_H_ */
+void MPU6050_Init(I2C_HandleTypeDef *hi2c, uint8_t GYRO_MODE, uint8_t ACCEL_MODE);        // Initializing...
+void MPU_6050_POSITION(I2C_HandleTypeDef *hi2c, uint8_t raw_data[14], uint8_t GYRO_MODE); // using Complementary Filters
+#endif                                                                                    /* INC_MPU_6050_H_ */
